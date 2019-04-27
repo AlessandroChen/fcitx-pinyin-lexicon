0ca76eb4515f50bb4a3c78d6f14a30b0f08e5b66
AlexanderChen <alex_passenger@outlook.com>
2019-04-27 13:28:03 +0800

    fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fcitx-pinyin-lexicon
 转换搜狗细胞词库合并至fcitx词库脚本
+
+# 安装
+首先到[搜狗细胞词库](https://pinyin.sogou.com/dict/)下载你需要用到的词库，到`~/Downloads/`目录下
+
+然后，`clone`我上传好的文件和脚本
+```bash
+$ git clone https://github.com/AlessandroChen/fcitx-pinyin-lexicon.git
+```
+在`fcitx-pinyin-lexicon`目录下直接运行 `install.sh` 即可
+
--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ mkdir tmp && cd tmp
 mv ~/Downloads/*.scel ./
 mkdir org
 for i in *.scel; do scel2org $i -o org/$i.org    ; done
-cp ../fcitx-pinyin-lexicon/pyPhrase.org org/
+cp ../pyPhrase.org org/
 mkdir dict && cd dict
 cat ../org/* | sort | uniq > ultimate.org
 createPYMB ../../gbkpy.org ultimate.org
-mv pyphrase.mb ~/.config/fcitx/pinyin/
-mv pybase.mb ~/.config/fcitx/pinyin/
+cp pyphrase.mb ~/.config/fcitx/pinyin/
+cp pybase.mb ~/.config/fcitx/pinyin/
